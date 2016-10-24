c1a8ed5dc9a12812168ff34b95d84e8484ba5a18
Andrew Hodge <andrew.hodge@teamaol.com>
2016-10-24 15:28:34 -0700

    Added boilerplate based on discord.io example

---
+++ index.js
@@ -0,0 +1,18 @@
+var Discord = require('discord.io');
+var bot = new Discord.Client({
+    token: "",
+    autorun: true
+});
+
+bot.on('ready', function() {
+    console.log(bot.username + " - (" + bot.id + ")");
+});
+
+bot.on('message', function(user, userID, channelID, message, event) {
+    if (message === "ping") {
+        bot.sendMessage({
+            to: channelID,
+            message: "pong"
+        });
+    }
+});
