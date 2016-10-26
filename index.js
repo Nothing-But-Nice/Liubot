c9d85cc49bd9cc0eb7808c43d18445ca906c1fea
Andrew Hodge <andrew.hodge@teamaol.com>
2016-10-26 13:13:44 -0700

    Updated error message to be more user friendly

--- index.js
+++ index.js
@@ -2,10 +2,23 @@
 var Discord = require('discord.io');
 var Converter = require("csvtojson").Converter;
 var converter = new Converter({});
+var _FUSE = require('fuse.js');
+var fuse;
 
 //Configurable Paramaters
 var BOT_TOKEN = require('./BOT-TOKEN.json');
 var BOT_ACTIVATION_STRING = '!lucy';
+var FUSE_OPTIONS = {
+    caseSensitive: false,
+    shouldSort: true,
+    threshold: 0.2,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    keys: [
+        "name"
+    ]
+};
 
 var ITEM_LIST = [];
 
@@ -17,6 +30,7 @@ converter.fromFile("./itemlist.txt",function(err,result){
     } else {
         ITEM_LIST = result;
         console.log("I've loaded the file and it has: " + ITEM_LIST.length + " items.");
+        fuse = new _FUSE(ITEM_LIST, FUSE_OPTIONS);
     }
 });
 
@@ -35,10 +49,14 @@ bot.on('message', function(user, userID, channelID, message, event) {
     var bot_string_regex = new RegExp(BOT_ACTIVATION_STRING, 'i');
     if (bot_string_regex.test(message)) {
         var item_lookup_string = message.replace(BOT_ACTIVATION_STRING + " ", "");
-        var found_item = {};
-        var reply_message = "I am broken.";
-        if(found_item) {
-            reply_message = found_item.lucylink;
+        var found_items = fuse.search(item_lookup_string);
+        var reply_message = "I was unable to find what you were looking for, please try again. ";
+        if(found_items.length) {
+            reply_message = "I found " + found_items.length + " items that match.  Displaying top results:";
+            var top_results_count = found_items.length > 3 ? 3 : found_items.length;
+            for(var x = 0; x < top_results_count; x++) {
+                reply_message += "\n" + found_items[x].name + " : " + found_items[x].lucylink;
+            }
         }
         bot.sendMessage({
             to: channelID,
