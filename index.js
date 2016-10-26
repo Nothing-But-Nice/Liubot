795257838ed591aa5010dee9087cc217c008eded
Andrew Hodge <andrew.hodge@teamaol.com>
2016-10-26 11:58:06 -0700

    Added csvtojson parsing and cleaned up some introduction messages

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
+//External Dependencies
 var Discord = require('discord.io');
+var Converter = require("csvtojson").Converter;
+var converter = new Converter({});
+
+//Configurable Paramaters
 var BOT_TOKEN = require('./BOT-TOKEN.json');
-var ITEM_LIST = require('./itemlist.json');
+var BOT_ACTIVATION_STRING = '!lucy';
+
+var ITEM_LIST = [];
+
+converter.fromFile("./itemlist.txt",function(err,result){
+    console.log("Converting csv to json...");
+    if(err) {
+        console.log(err);
+        process.exit(-1);
+    } else {
+        ITEM_LIST = result;
+        console.log("I've loaded the file and it has: " + ITEM_LIST.length + " items.");
+    }
+});
+
 var bot = new Discord.Client({
     token: BOT_TOKEN.token,
     autorun: true
 });
-var bot_activation_string = '!lucy';
-var ITEM_LIST_KEYS = Object.keys(ITEM_LIST);
 
 bot.on('ready', function() {
-    console.log(bot.username + " - (" + bot.id + ")");
-    console.log("To share this bot please use the following URL: https://discordapp.com/oauth2/authorize?&client_id=" + bot.id + "&scope=bot&permissions=0");
-    console.log("I've loaded the file and it has: " + ITEM_LIST_KEYS.length + " items.");
+    console.log(bot.username + " has begun operation.");
+    console.log("To share this bot please use the following URL: \n==========\nhttps://discordapp.com/oauth2/authorize?&client_id=" + bot.id + "&scope=bot&permissions=0\n==========");
 });
 
 bot.on('message', function(user, userID, channelID, message, event) {
 
-    var bot_string_regex = new RegExp(bot_activation_string, 'i');
+    var bot_string_regex = new RegExp(BOT_ACTIVATION_STRING, 'i');
     if (bot_string_regex.test(message)) {
-        var item_lookup_string = message.replace(bot_activation_string + " ", "");
-        var found_item = ITEM_LIST[item_lookup_string];
-        // console.log(found_item);
-        var reply_message = "I can't seem to find '" + item_lookup_string + "', perhaps you wanted '" + ITEM_LIST_KEYS[getRandomIntInclusive(0, ITEM_LIST_KEYS.length-1)] + "' instead?";
+        var item_lookup_string = message.replace(BOT_ACTIVATION_STRING + " ", "");
+        var found_item = {};
+        var reply_message = "I am broken.";
         if(found_item) {
-            reply_message = "Is this what you are looking for? " + found_item.url;
+            reply_message = found_item.lucylink;
         }
         bot.sendMessage({
             to: channelID,
