f512e0785a4485e62a5f9e90eb05730713da0d2d
Andrew Hodge <andrew.hodge@teamaol.com>
2016-10-27 10:59:17 -0700

    Basic web scraping of lucy implementation.  Will parse site and get the data for item that lucy displays.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 //External Dependencies
+var request = require('request');
+var cheerio = require('cheerio');
 var Discord = require('discord.io');
 var Converter = require("csvtojson").Converter;
 var converter = new Converter({});
@@ -7,6 +9,7 @@ var fuse;
 
 //Configurable Paramaters
 var BOT_TOKEN = require('./BOT-TOKEN.json');
+var LUCY_ITEM_URL = "http://lucy.allakhazam.com/item.html?id=";
 var BOT_ACTIVATION_STRING = '!lucy';
 var FUSE_OPTIONS = {
     caseSensitive: false,
@@ -41,7 +44,10 @@ var bot = new Discord.Client({
 
 bot.on('ready', function() {
     console.log(bot.username + " has begun operation.");
-    console.log("To share this bot please use the following URL: \n==========\nhttps://discordapp.com/oauth2/authorize?&client_id=" + bot.id + "&scope=bot&permissions=0\n==========");
+    console.log("To share this bot please use the following URL: \n" +
+        "==========\n" +
+        "https://discordapp.com/oauth2/authorize?&client_id=" + bot.id + "&scope=bot&permissions=0\n" +
+        "==========");
 });
 
 bot.on('message', function(user, userID, channelID, message, event) {
@@ -49,6 +55,7 @@ bot.on('message', function(user, userID, channelID, message, event) {
     var bot_string_regex = new RegExp(BOT_ACTIVATION_STRING, 'i');
     if (bot_string_regex.test(message)) {
         var item_lookup_string = message.replace(BOT_ACTIVATION_STRING, "");
+        var item_info_message;
         console.log(item_lookup_string);
         var found_items = fuse.search(item_lookup_string);
         var reply_message = "I was unable to find what you were looking for, please try again. ";
@@ -58,10 +65,13 @@ bot.on('message', function(user, userID, channelID, message, event) {
             for(var x = 0; x < top_results_count; x++) {
                 reply_message += "\n" + found_items[x].name + " : " + found_items[x].lucylink;
             }
+            // item_info_message = "Here is information for " + 
+            //     found_items[0].name + 
+            //     "\n" + getDataFromLucy(found_items[0].id);
         }
         bot.sendMessage({
             to: channelID,
-            message: reply_message
+            message: reply_message + item_info_message
         });
     }
 });
@@ -70,4 +80,31 @@ function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function getDataFromLucy(id) {
+    var options = {
+        url: LUCY_ITEM_URL + id,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+            "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding":"gzip, deflate, sdch",
+            "Accept-Language":"en-US,en;q=0.8",
+            "Cache-Control":"max-age=0",
+            "Connection":"keep-alive",
+            "Cookie":"",
+            "DNT":1,
+            "Host":"lucy.allakhazam.com",
+            "Referer": LUCY_ITEM_URL + id,
+            "Upgrade-Insecure-Requests":1
+        }
+    };
+    request(options, function(error, response, html){
+        if(!error){
+            var $ = cheerio.load(html);
+            return $('.shotdata').text();
+        }
+    });
+    return;
 }
