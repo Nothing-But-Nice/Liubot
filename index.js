84e9ad1220e43b2c622bf0c76047a69d21614c83
Andrew Hodge <andrew.hodge@teamaol.com>
2016-11-07 09:56:13 -0800

    Cleaned up parser to work properly and return a formatted message to chat of first item that matcheS

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ var BOT_ACTIVATION_STRING = '!lucy';
 var FUSE_OPTIONS = {
     caseSensitive: false,
     shouldSort: true,
-    threshold: 0.2,
+    // tokenize: true,
+    threshold: 0.15,
     location: 0,
-    distance: 100,
+    distance: 10,
     maxPatternLength: 32,
     keys: [
         "name"
@@ -52,11 +53,10 @@ bot.on('ready', function() {
 
 bot.on('message', function(user, userID, channelID, message, event) {
 
-    var bot_string_regex = new RegExp(BOT_ACTIVATION_STRING, 'i');
+    var bot_string_regex = new RegExp(BOT_ACTIVATION_STRING, 'gi');
     if (bot_string_regex.test(message)) {
         var item_lookup_string = message.replace(BOT_ACTIVATION_STRING, "");
-        var item_info_message;
-        console.log(item_lookup_string);
+        console.log("Searching for: ", item_lookup_string);
         var found_items = fuse.search(item_lookup_string);
         var reply_message = "I was unable to find what you were looking for, please try again. ";
         if(found_items.length) {
@@ -65,46 +65,50 @@ bot.on('message', function(user, userID, channelID, message, event) {
             for(var x = 0; x < top_results_count; x++) {
                 reply_message += "\n" + found_items[x].name + " : " + found_items[x].lucylink;
             }
-            // item_info_message = "Here is information for " + 
-            //     found_items[0].name + 
-            //     "\n" + getDataFromLucy(found_items[0].id);
+            getDataFromLucyAndSendToChannel(found_items[0].id, found_items[0].name, channelID);
         }
         bot.sendMessage({
             to: channelID,
-            message: reply_message + item_info_message
+            message: reply_message
         });
     }
 });
 
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-
-function getDataFromLucy(id) {
+function getDataFromLucyAndSendToChannel(itemId, itemName, channelID) {
     var options = {
-        url: LUCY_ITEM_URL + id,
+        url: LUCY_ITEM_URL + itemId,
         headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
             "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
             "Accept-Encoding":"gzip, deflate, sdch",
             "Accept-Language":"en-US,en;q=0.8",
             "Cache-Control":"max-age=0",
             "Connection":"keep-alive",
-            "Cookie":"",
+            "Cookie":"__qca=P0-771874657-1469654866999; __gads=ID=7401942ee4b36263:T=1469726292:S=ALNI_MaAu7MQVA771x8n92DCRlUx1kxhxQ; LucySessionID=67.170.11.131.1476676558548614; _ga=GA1.2.280683370.1469654867; __utmt=1; __utma=187107893.280683370.1469654867.1477525882.1477591816.16; __utmb=187107893.1.10.1477591816; __utmc=187107893; __utmz=187107893.1477519636.13.7.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)",
             "DNT":1,
             "Host":"lucy.allakhazam.com",
-            "Referer": LUCY_ITEM_URL + id,
-            "Upgrade-Insecure-Requests":1
+            "Upgrade-Insecure-Requests":1,
+            "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
         }
     };
     request(options, function(error, response, html){
         if(!error){
             var $ = cheerio.load(html);
-            return $('.shotdata').text();
+            var parsed_text = $('.shotdata').text();
+            var formatted_message = "```markdown\n" + 
+                itemName + 
+                "\n---\n" +
+                parsed_text.trim() +
+                "\n```";
+            bot.sendMessage({
+                to: channelID,
+                message: formatted_message
+            });
+        } else {
+            console.log("ERROR: " + error);
+            bot.sendMessage({
+                to: channelID,
+                message: error
+            });
         }
     });
-    return;
 }
